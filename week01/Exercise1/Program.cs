5e15c6fe7061fd49032327f18dcbde30d96384c4
Redactbtw <officialredact@gmail.com>
2025-11-03 18:03:44 +0200

    Completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,17 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
         Console.WriteLine("What is your first name? ");
         string first = Console.ReadLine();
 
         Console.WriteLine("What is your last name? ");
-=======
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.Write("What is your last name? ");
->>>>>>> a836016d18840a798326f0c721efc49aabaa3ba8
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
