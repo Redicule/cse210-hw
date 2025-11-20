d326c004cb2df330ebbe6a3aef7dc345162e7ad8
Redactbtw <officialredact@gmail.com>
2025-11-20 22:16:44 +0200

    Completed.

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        _random = new Random();
+
+        string[] splitWords = text.Split(" ");
+        foreach (string w in splitWords)
+        {
+            _words.Add(new Word(w));
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+
+        return $"{_reference.GetDisplayText()}\n\n{string.Join(" ", displayWords)}";
+    }
+
+    public void HideRandomWords()
+    {
+        int numberToHide = 3; // hide 3 at a time
+        int hiddenCount = 0;
+
+        while (hiddenCount < numberToHide && !AreAllWordsHidden())
+        {
+            int index = _random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public bool AreAllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
